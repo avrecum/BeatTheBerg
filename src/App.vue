837ad263852e3922ac915911f6d246622c747c8e
Felix Wu <mail@flxwu.com>
2018-11-07 09:10:32 +0100

    Initial Commit 🎉

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <a-scene embedded artoolkit="webcam">
+  	<a-marker-camera preset="hiro">
+      <a-box position='0 0.5 0' material='color: black'></a-box>
+  	</a-marker-camera>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="sass">
+body
+  margin: 0
+  overflow: hidden
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
