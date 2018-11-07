1bf65294fd001e002e583158da56a67bbcadfcc2
Linda Thelen <linda.thelen@arcor.de>
2018-11-07 12:13:39 +0100

    Add files via upload

---
+++ Countdown/main.js
@@ -0,0 +1,33 @@
+// Set the date we're counting down to
+
+var countDownDate = new Date("2018-11-09T14:00:00");
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    if(countDownDate == null){
+        document.getElementById("timer").innerHTML = "";
+    }
+    else{
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementsByTagName("main")[0].innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    document.getElementsByTagName("main")[0].innerHTML = "0d 0h 0m 0s";
+  }
+    }
+}, 1000);
