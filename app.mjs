45de310ecfa4be7e8b615bbdef6907f0be407c4f
Quentin Oschatz <quentin@cliffhouse.de>
2018-11-08 13:38:34 +0100

    Upgraded to Firebase Admin

--- app.mjs
+++ app.mjs
@@ -1,53 +1,58 @@
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import debug from 'debug';
-debug('beattheberg:server');
-import http from 'http';
-import firebase from 'firebase';
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import debug from "debug";
+debug("beattheberg:server");
+import http from "http";
+import admin from "firebase-admin";
 
-import config from './config.js';
+import serviceAccount from "./secrets.json";
 
-import { getRouter } from './routes/api.mjs';
-import head from './views/head.mjs';
+import config from "./config.js";
+
+import { getRouter } from "./routes/api.mjs";
+import head from "./views/head.mjs";
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'ejs');
-app.use(express.static(path.resolve() + '/public'));
+app.set("view engine", "ejs");
+app.use(express.static(path.resolve() + "/public"));
 
 // index page
-app.get('/', function(req, res) {
-  res.render('pages/index', {
+app.get("/", function(req, res) {
+  res.render("pages/index", {
     head_template: head
   });
 });
 
 // game page
-app.get('/game', function(req, res) {
-  res.render('pages/game', {
+app.get("/game", function(req, res) {
+  res.render("pages/game", {
     head_template: head
   });
 });
 
 // game page
-app.get('/leaderboard', function(req, res) {
-  res.render('pages/leaderboard', {
+app.get("/leaderboard", function(req, res) {
+  res.render("pages/leaderboard", {
     head_template: head
   });
 });
 
 // Init firebase
-const ref = firebase.initializeApp(config);
+const ref = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://beat-the-berg.firebaseio.com"
+});
 
 // Router
-app.use('/api/', getRouter(ref));
+app.use("/api/", getRouter(ref));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,7 +63,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.json({ error: err.message });
@@ -68,8 +73,8 @@ app.use(function(err, req, res, next) {
  * Get port from environment and store in Express.
  */
 
-const port = process.env.PORT || '5000';
-app.set('port', port);
+const port = process.env.PORT || "5000";
+app.set("port", port);
 
 /**
  * Create HTTP server, listen on ports
@@ -77,27 +82,27 @@ app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Event listener for HTTP server "error" event.
  */
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -110,6 +115,6 @@ function onError(error) {
  */
 function onListening() {
   let addr = server.address();
-  let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  let bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
