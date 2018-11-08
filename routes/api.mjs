541865217af2e14370559a0abbf5bf1288d8cf9f
Felix Wu <mail@flxwu.com>
2018-11-08 11:52:10 +0100

    finish login/register endpoints

--- routes/api.mjs
+++ routes/api.mjs
@@ -6,7 +6,7 @@ let leaderboardDB;
 let userDB;
 let firebase;
 
-export const getRouter = (firebaseRef) => {
+export const getRouter = firebaseRef => {
   firebase = firebaseRef;
   database = firebase.database();
   userDB = database.ref('users');
@@ -21,7 +21,6 @@ export const getRouter = (firebaseRef) => {
  *         user rank as int
  */
 router.get('/leaderboard', async (req, res, next) => {
-
   let name = req.query.name || null;
   console.log(name);
 
@@ -29,56 +28,61 @@ router.get('/leaderboard', async (req, res, next) => {
   let rank;
   let user;
 
-  const getData = async (data) => {
-    if(data.val()) {
+  const getData = async data => {
+    if (data.val()) {
       let tmp = await data.val();
       leaderboard.push(Object.values(tmp));
-      leaderboard = leaderboard.sort((a, b) => parseFloat(b.time) - parseFloat(a.time)).slice(0, 20);
-      user = leaderboard[0].find((el) => el.name === name);
-      rank = leaderboard[0].indexOf(leaderboard[0].sort((a, b) => parseFloat(a.time) - parseFloat(b.time)).find((el) => el.name === name));
+      leaderboard = leaderboard
+        .sort((a, b) => parseFloat(b.time) - parseFloat(a.time))
+        .slice(0, 20);
+      user = leaderboard[0].find(el => el.name === name);
+      rank = leaderboard[0].indexOf(
+        leaderboard[0]
+          .sort((a, b) => parseFloat(a.time) - parseFloat(b.time))
+          .find(el => el.name === name)
+      );
       rank++;
       return true;
     } else {
-      res.json({ status: 500, err: "No data! " });
+      res.json({ status: 500, err: 'No data! ' });
       return false;
     }
-  }
+  };
 
-  const errData = (error) => {
+  const errData = error => {
     console.error('Something went wrong.');
     console.error(error);
-  }
+  };
 
   let dataList = await leaderboardDB.on('value', getData, errData);
 
   // Return project if availible
-  if(dataList) {
-    res.json({ status: 200, data: { leaderboard: leaderboard, user: user, rank: rank } });
-  } else
-    res.json({ status: 500, err: "Error while getting leaderboard" });
-
+  if (dataList) {
+    res.json({
+      status: 200,
+      data: { leaderboard: leaderboard, user: user, rank: rank }
+    });
+  } else res.json({ status: 500, err: 'Error while getting leaderboard' });
 });
 
 /**
  * POST Leaderboard
  */
 router.post('/leaderboard', (req, res, next) => {
-
   let name = req.body.name;
   let time = Number(req.body.time);
 
   let user = {
-      name: name,
-      time: time
+    name: name,
+    time: time
   };
 
-  let dbLeaderboardEntry = leaderboardDB.push(user, (a) => console.log(a));
+  let dbLeaderboardEntry = leaderboardDB.push(user, a => console.log(a));
   console.log('Data: Firebase generated key: ' + dbLeaderboardEntry.key);
 
-  if(dbLeaderboardEntry)
+  if (dbLeaderboardEntry)
     res.json({ status: 200, data: dbLeaderboardEntry.key });
-  else
-    res.json({ status: 500, err: "Error while adding leaderboard entry!" });
+  else res.json({ status: 500, err: 'Error while adding leaderboard entry!' });
 });
 
 /**
@@ -94,24 +98,28 @@ router.get('/user', (req, res, next) => {
 router.post('/user/login', (req, res) => {
   const user = req.body.user;
   try {
-    userDB.on('value', (data) => {
+    userDB.on('value', data => {
       let users = data.val();
       if (users) {
         if (users[user]) {
-          
+          res.json({
+            status: 200,
+            data: {
+              progress: users[user].progressCounter,
+              startTime: users[user].startTime
+            }
+          });
+        } else {
+          res.json({
+            status: 500,
+            data: "User not in database"
+          });
         }
       }
     });
-    let dbUserEntry = userDB.child(user).set({
-      startTime: Date.now(),
-      progressCounter: 0
-    });
-    console.log('Data: Firebase generated key: ' + dbUserEntry.key);
   } catch (err) {
     console.error(err);
   }
-
-  res.redirect('/game');
 });
 
 /**
@@ -120,13 +128,27 @@ router.post('/user/login', (req, res) => {
 router.post('/user/register', (req, res) => {
   const user = req.body.user;
   try {
-    
-    let dbUserEntry = userDB.child(user).set({
-      startTime: Date.now(),
-      progressCounter: 0
+    userDB.on('value', data => {
+      let users = data.val();
+      if (users) {
+        if (users[user]) {
+          res.json({
+            status: 200,
+            data: {
+              progress: users[user].progressCounter,
+              startTime: users[user].startTime
+            }
+          });
+        } else {
+          let dbUserEntry = userDB.child(user).set({
+            startTime: Date.now(),
+            progressCounter: 0
+          });
+          console.log('Data: Firebase generated key: ' + dbUserEntry.key);      
+        }
+      }
     });
-    console.log('Data: Firebase generated key: ' + dbUserEntry.key);
-  } catch (err) {
+    } catch (err) {
     console.error(err);
   }
 
