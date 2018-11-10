ad38f19e80a14702bb4457f417594cece952d737
Linda Thelen <linda.thelen@arcor.de>
2018-11-10 15:50:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
 
-var countDownDate = new Date("2018-11-10T16:15:00");
+var countDownDate = new Date("2018-11-10T18:00:00");
 
 // Update the count down every 1 second
 var x = setInterval(function() {
